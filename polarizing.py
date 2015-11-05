678d7b501a5ecb40e410141b3030d53a89d20d72
Peixian Wang <peixian@plains.guru>
2015-11-05 10:12:09 -0600

    Added the card sheets and the python file

--- polarizing.py
+++ polarizing.py
@@ -1,4 +1,36 @@
+#!/usr/bin/env python3
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+#reads the filename, spits out a dataframe
+#   
+def parse(filename):
+    return pd.read_excel(filename)
+
+#emperical baysian analysis on the dataframe
+
+
+#main method
+
+if __name__ == '__main__':
+    from sys import argv, exit
+    usage = """
+    USAGE: python {0} filesheet
+    
+    WHERE: filesheet is the name of the xlsx of interest
+    (note that this is currently only designed for the sheet of bfz that was posted on /r/lrcast, see source.txt for more details)
+    """
+    
+    if (len(argv) != 2):
+        print(usage.format(argv[0]))
+        
+    else:
+        try: 
+            df = parse(argv[1])
+            print(df) 
+            
+        except IOError:
+            print("File doesn't exist.\n")
+            exit(-1)
---
+++ source.md
@@ -0,0 +1,3 @@
+# November 5, 2015 
+- Data grabbed from https://www.reddit.com/r/lrcast/comments/3rk3xg/bfz_mtgo_draft_statistics/
+- Changed data: Devoid cards were changed to have a color, previously they did not have a color in the spreadsheet
