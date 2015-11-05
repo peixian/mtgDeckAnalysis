bc6dc5c09ff5b8c8db62a09361ded5aad2305372
Peixian Wang <peixian@plains.guru>
2015-11-05 15:34:44 -0600

    Finished?

--- polarizing.py
+++ polarizing.py
@@ -3,40 +3,94 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import json 
+import matplotlib.cm as cm
 from pprint import pprint 
+import scipy.stats as stats
+import warnings
 
-#reads the filename, adds the colors, spits out a dataframe
+#reads the filename, spits out a dataframe
 #   
 def parse(filename):
-    df = pd.read_csv(filename)
-    with open("BFZ.json") as json_data:
-        data = json.load(json_data)
+    return pd.read_csv(filename, usecols=["Card", "Win", "Loss", "Total", "Win%", "Rarity", "CC", "Color"])
+
+#emperical baysian analysis on the dataframe, spits out a dataframe sorted by the best card
+def fit(df):
+    #need to find the a distribution that fits the data
+
+    winPercent = df[df.Total >= 0]
+    print len(winPercent)
+    #histogram is relatively normal, so we can use a beta distribution
+    (a, b, loc, scale) = stats.beta.fit(winPercent["Win%"], loc = np.mean(winPercent["Win%"]))
+    x = np.linspace(0, 1, 100)
+    rv = stats.beta(a, b, loc, scale)
+    
+    #fig, ax = plt.subplots()
+    #ax.hist(winPercent["Win%"], bins = 200)
+    #ax.plot(x, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
+    #ax.set_xlabel("Win%")
+    #ax.set_ylabel("Density")
+    
+    
+    #plt.show()
+    #not perfect, but close enough?
     
    
+    
+    df["ebWin%"] = df[["Win", "Total"]].apply(lambda x: (x[0] + a)/(x[1] + a + b), axis=1)
+    # df["alpha1"] = df[["Win"]].apply(lambda x: x[0]+a, axis = 1)
+    # df["beta1"] = df[["Win", "Total"]].apply(lambda x: x[1] - x[0] + b, axis = 1)
+    # print((df["Win"][:3], df["Total"][:3]))
+    # print((df["Win%"][:3]))
+    # print(df["ebWin%"][:3])
+    return (df.sort("ebWin%", ascending=False))
+    
+def analyze(df, color = "All Colors", rarity = "All rarities", top = 50):
+    if (color == "All Colors" and rarity == "All rarities"):
+        df = df
+    elif (color == "All Colors"):
+        df = df[df.Rarity == rarity]
+    elif (rarity == "All rarities"):
+        df = df[df.Color == color]
+    else:
+        df = df[(df.Color == color) & (df.Rarity == rarity)]
+    df = df[:top].sort("ebWin%", ascending=False)
+    # d["low"] = d[["alpha1", "beta1"]].apply(lambda x: stats.beta.ppf(.025, x["alpha1"], x["beta1"]), axis = 1)
+    # d["high"] = d[["alpha1", "beta1"]].apply(lambda x: stats.beta.ppf(.975, x["alpha1"], x["beta1"]), axis = 1)
+    print(df)
+   
+    fig, ax = plt.subplots()
+    ax.set_title("BFZ Top {} for {} at {}".format(top, color, rarity))
+    sns.set_style("whitegrid")
 
-#emperical baysian analysis on the dataframe
-
+    ax = sns.stripplot(x = "ebWin%", y="Card", hue="Color",data=df,orient="h")
+    ax.legend_.remove()
+    plt.tight_layout()
+    plt.savefig("Top_{}_for_{}_at_{}.pdf".format(top, color, rarity))
+    
 
 #main method
-
-if __name__ == '__main__':
-    from sys import argv, exit
-    usage = """
-    USAGE: python {0} filesheet
-    
-    WHERE: filesheet is the name of the xlsx of interest
-    (note that this is currently only designed for the sheet of bfz that was posted on /r/lrcast, see source.txt for more details)
-    """
-    
-    if (len(argv) != 2):
-        print(usage.format(argv[0]))
+#yes I write bad code, thanks for pointing it out python
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    if __name__ == '__main__':
+        from sys import argv, exit
+        usage = """
+        USAGE: python {0} filesheet
         
-    else:
-        try: 
-            df = parse(argv[1])
-            #print(df) 
-            
-        except IOError:
-            print("File doesn't exist.\n")
-            exit(-1)
+        WHERE: filesheet is the name of the xlsx of interest
+        (note that this is currently only designed for the sheet of bfz that was posted on /r/lrcast, see source.txt for more details)
+        """
+        
+        if (len(argv) != 2):
+            print(usage.format(argv[0]))
+        else:
+            try: 
+                df = parse(argv[1])
+                cList = ["White", "Blue", "Black", "Red", "Green", "0"]
+                rList = ["C", "U", "R", "M"]
+                for c in cList:
+                    for r in rList:
+                        analyze(fit(df), color = c, rarity = r, top = 15)
+            except IOError:
+                print("File doesn't exist.\n")
+                exit(-1)
